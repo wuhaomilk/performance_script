eeba1dcef5d47325fe65f79e37eb546d1996a2ae
wuhao <clayderman@yardbot.net>
2024-04-07 16:31:45 +0800

    update

--- hj_rosbag/run_record_processed_topic.sh
+++ hj_rosbag/run_record_processed_topic.sh
@@ -12,13 +12,21 @@ fi
 DATE=`date +%Y%m%d`
 ROSBAG_STORE_PATH=$ROSBAG_STORE_PATH/$DATE
 echo "the current rosbag save path:$ROSBAG_STORE_PATH"
-mkdir $ROSBAG_STORE_PATH
-cd $ROSBAG_STORE_PATH
-# TOPIC_LIST="/bat_chatter /depth_chatter /ulsound_chatter /turbidity_data /soc_imu_chatter /imu_chatter /motor_chatter "
-# TOPIC_LIST=$TOPIC_LIST" tf  "
+
+if [ ! -d "$ROSBAG_STORE_PATH" ] ;then
+    mkdir -p $ROSBAG_STORE_PATH
+fi
+
+relative_path=$(dirname $0)
+ablsolute_path=`pwd`
+excute_path="$ablsolute_path/$relative_path"
+
+
+# echo $excute_path
+# cd $ROSBAG_STORE_PATH
 now=$(date +"%Y-%m-%d-%H-%M-%S")
 rosbag_name=$now.bag
 
+cd $excute_path
 #./hj_rosbag record -a --split --size=500 --max-splits=5 -O $rosbag_name   # 500M split.最多存5个文件
-./hj_rosbag record -a --min-space=500M -O $rosbag_name
-
+./hj_rosbag record -a --min-space=500M -O $ROSBAG_STORE_PATH/$rosbag_name
